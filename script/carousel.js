7ab1b52fa797dfae6b2ca25a49b2e33169e12eb4
Elias Duvernois <e.duvernois2016@gmail.com>
2023-01-29 18:10:37 +0100

    Add of the form with his js intruction

--- script/carousel.js
+++ script/carousel.js
@@ -1,5 +1,37 @@
 const btnL = document.getElementsByClassName('btn_left');
 const btnR = document.getElementsByClassName('btn_right');
 
+/* cont from */
 
-console.log(btnL);
+const Name = document.getElementById('name');
+const Email = document.getElementById('email');
+const Mess = document.getElementById('message');
+const Btn_send = document.getElementById("send");
+
+console.log(Name);
+const Form_need =[Name,Email,Mess]
+
+function TurnNormal(){
+    for(const needs of Form_need){
+        if(needs.style.borderColor === "red"){
+            console.log("Elias")
+           if(needs.value.length > 0){
+                console.log("Elias")
+                needs.style.borderColor = "White";
+           }
+        }
+    }
+}
+
+Btn_send.addEventListener("click", (e)=>{
+    for(const needs of Form_need){
+        if(needs.value == ""){
+            e.preventDefault();
+            needs.style.borderColor ="red";
+        }
+    }
+})
+
+for( const needs of Form_need){
+    needs.addEventListener("change",TurnNormal)
+}
